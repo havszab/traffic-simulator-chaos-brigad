394c1de008cf8aa90be6617891bb3aa8799af552
havszab <havaldaszab@gmail.com>
2018-11-07 10:47:28 +0100

    app start bug fixed

--- src/simulation/Main.java
+++ src/simulation/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../sample.fxml"));
+        Simulation simulation = new Simulation();
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(simulation, 300, 275));
         primaryStage.show();
     }
 
